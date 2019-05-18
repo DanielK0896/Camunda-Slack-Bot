897018a3aad54b1777cd6937d376803edbbb5ccd
Daniel Köster <danielk.privat@hotmail.de>
2019-05-19 01:48:38 +0200

    update

--- api/controllers/startProcess.js
+++ api/controllers/startProcess.js
@@ -5,7 +5,7 @@ var trainer = "Daniel";
 var room = "";
 var date = "2019-05-29T16:00:00";
 var businessKey = name + date;
-var path = "/engine-rest/process-definition/key/hr_schulung/start";
+var path = "/camunda/startProcess";
 
 var payload = JSON.stringify({ name: name, trainer: trainer, room: room, date: date, businessKey: businessKey });
-mod.postJsonToLocalhost(payload, 8080, path);
+mod.postJsonToLocalhost(payload, 10010, path);
