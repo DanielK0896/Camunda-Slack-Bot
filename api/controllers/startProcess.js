a4a0913c82da92a93b8174348e18d7369748f2ee
Daniel Köster <danielk.privat@hotmail.de>
2019-05-19 01:34:48 +0200

    update

--- api/controllers/startProcess.js
+++ api/controllers/startProcess.js
@@ -7,5 +7,5 @@ var date = "2019-05-29T16:00:00";
 var businessKey = name + date;
 var path = "/engine-rest/process-definition/key/hr_schulung/start";
 
-var payload = JSON.stringify({ variable1: name, variable2: trainer, variable3: room, variable4: date, businessKey: businessKey });
+var payload = JSON.stringify({ name: name, trainer: trainer, room: room, date: date, businessKey: businessKey });
 mod.postJsonToLocalhost(payload, 8080, path);
