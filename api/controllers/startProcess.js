bf2cc2793e134a8cbd76a5b003bacd044bdb32f3
Daniel Köster <danielk.privat@hotmail.de>
2019-05-19 14:22:09 +0200

    update

--- api/controllers/startProcess.js
+++ api/controllers/startProcess.js
@@ -1,7 +1,7 @@
 const mod = require('./modules');
 
 var name = "QM Schulung";
-var trainer = "Daniel";
+var trainer = 'Daniel';
 var room = "";
 var date = "2019-05-29T16:00:00";
 var businessKey = name + date;
