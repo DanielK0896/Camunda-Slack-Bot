4ed6aa2789f511aee7a548804cad52c186cd7749
Daniel Köster <danielk.privat@hotmail.de>
2019-05-19 17:18:04 +0200

    update

--- api/controllers/startProcess.js
+++ api/controllers/startProcess.js
@@ -3,7 +3,7 @@ const mod = require('./modules');
 var name = "QM Schulung";
 var trainer = "Daniel";
 var room = "";
-var date = "2019-05-29T16:00:00+0200";
+var date = "2019-05-29T16:00:00.000+0200";
 var businessKey = name + date;
 var path = "/camunda/startProcess/schulungOrganisieren";
 
