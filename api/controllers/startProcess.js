792c12310b77f6cd746866e2ff9076a6c0baa05e
Daniel Köster <danielk.privat@hotmail.de>
2019-05-19 14:23:04 +0200

    update

--- api/controllers/startProcess.js
+++ api/controllers/startProcess.js
@@ -1,11 +1,11 @@
 const mod = require('./modules');
 
 var name = "QM Schulung";
-var trainer = 'Daniel';
+var trainer = "Daniel";
 var room = "";
 var date = "2019-05-29T16:00:00";
 var businessKey = name + date;
 var path = "/camunda/startProcess";
 
-var payload = JSON.stringify({ variable1: name, variable2: trainer, variable3: date, businessKey: businessKey });
+var payload = JSON.stringify({ variable1: name, variable2: "Daniel", variable3: date, businessKey: businessKey });
 mod.postToSwaggerAPI(payload, path);
