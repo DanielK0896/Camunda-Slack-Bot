9b32233a4abe4922b7f2e6e6345d0e50817019dd
Daniel Köster <danielk.privat@hotmail.de>
2019-05-19 13:55:37 +0200

    update

--- api/controllers/startProcess.js
+++ api/controllers/startProcess.js
@@ -8,4 +8,4 @@ var businessKey = name + date;
 var path = "/camunda/startProcess";
 
 var payload = JSON.stringify({ variable1: name, variable2: trainer, variable3: room, variable4: date, businessKey: businessKey });
-mod.postJsonToLocalhost(payload, 10010, path);
+mod.postToSwaggerAPI(payload, path);
