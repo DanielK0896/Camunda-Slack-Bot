87e506fe374badd03e3433cbf5b4677f289f61a4
Daniel Köster <danielk.privat@hotmail.de>
2019-05-21 14:42:52 +0200

    update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const { Client, logger } = require("camunda-external-task-client-js");
-const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
-const client = new Client(config);
+const configCamunda = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+const client = new Client(configCamunda);
 const { Variables } = require("camunda-external-task-client-js");
 const mod = require('./api/controllers/modules');
 
