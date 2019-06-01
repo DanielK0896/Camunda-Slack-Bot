35c76833f3d613bdc877c5d89b9b1e835613c0e9
Daniel Köster <danielk.privat@hotmail.de>
2019-06-01 10:38:50 +0200

    update

--- api/controllers/slackReceive.js
+++ api/controllers/slackReceive.js
@@ -34,9 +34,8 @@ function slackReceive(req, res) {                  //receive Slack POSTs after i
 
         var variableInformation = taskid[3].split(',');
         for (i = 1; i <= variableInformation.length; i++) {
-            var numberVariable = "variable." + (i - 1).toString();
             arrayOfVariables["nameVariable"[i]] = variableInformation[i - 1];
-            arrayOfVariables = (mod.pushSpecificVariables(arrayOfVariables, numberVariable, variableInformation[i - 1], msg)); // callbackId[3] = "variable1,variable2,..." e.g. "three,user,user.name"
+            arrayOfVariables = (mod.pushSpecificVariables(arrayOfVariables, "variable", variableInformation[i - 1], msg)); // callbackId[3] = "variable1,variable2,..." e.g. "three,user,user.name"
             if (typeof pushedButton != "undefined") {
                 arrayOfVariables[numberVariable] = pushedButton + "," + arrayOfVariables[numberVariable];
             }
