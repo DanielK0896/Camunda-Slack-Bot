0d702e9dfc11cf0bc9f0ac903d7babf8fea693f3
Daniel Köster <danielk.privat@hotmail.de>
2019-06-02 23:02:58 +0200

    updateOverflow problem

--- api/controllers/slackReceive.js
+++ api/controllers/slackReceive.js
@@ -90,7 +90,9 @@ function slackReceive(req, res) {                  //receive Slack POSTs after i
                     }
                 }    
             }
+            
             payload["blocks"] = mod.pushSpecificVariables(payload["blocks"], changes[actionValue], (parseInt(actionValue, 10) + changes.length / 2).toString(), changes)
+            console.log(JSON.stringify(payload));
             mod.postToSwaggerAPI(JSON.stringify(payload), "/updateOverflow");
         } else if (msg.actions[0].type == "button" && msg.actions[0].action_id != "lastMessage") {
             payload["channel"] = msg.container.channel_id;
