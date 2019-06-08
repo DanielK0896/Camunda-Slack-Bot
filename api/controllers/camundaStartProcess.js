3ba71073ec067747c882c4b03a0627d6748109cb
Daniel Köster <danielk.privat@hotmail.de>
2019-06-08 12:42:21 +0200

    update

--- api/controllers/camundaStartProcess.js
+++ api/controllers/camundaStartProcess.js
@@ -1,57 +1,32 @@
 /* Example for starting a Camunda process via API passing required variables*/
 
-
-
 var request = require('request');
-var URL = "http://localhost:8080/engine-rest/process-definition/key/hr_schulung/start";
+var URL = "http://localhost:8080/engine-rest/process-definition/key/";
+var processes = require('./processes');
 
 module.exports = {
-    camundaStartProcessSchulungOrganisieren: camundaStartProcessSchulungOrganisieren
+    camundaStartProcess: camundaStartProcess
 };
 
-function camundaStartProcessSchulungOrganisieren(req, res) {
+function camundaStartProcess(req, res) {
 
+    var process = req.swagger.params.process.value;
+    URL += process + "/start"
     var msg = req.swagger.params.body.value;
-    var body = {
-        "variables": {
-            "name": {
-                "value": msg.name,
-                "type": "String"
-            },
-            "trainer": {
-                "value": msg.trainer,
-                "type": "String" 
-            },
-            "date": {
-                "value": msg.date,
-                "type": "Date"
-            },
-            "room": {
-                "value": msg.room,
-                "type": "String"
-            },
-            "process": {
-                "value": "schulungOrganisieren",
-                "type": "String"
-            },
-            "correlationKey": {
-                "value": "name,date",
-                "type": "String"
-            },
-            "listOfUserIds": {
-                "value": "SlackIds",
-                "type": "String"
-            }
-        },
-        "businessKey": msg.businessKey
-    };
+    var processIndex;
+    body = {};
+    for (processIndex in processes) {
+        if (typeof processes[processIndex][process] != "undefined") {
+            body.variables = processes[processIndex][process];
+        }
+    }
+    console.log(body);
     var options = {
         method: 'POST',
         url: URL,
         headers:
         {
             'cache-control': 'no-cache',
-            Authorization: secrets.Authorization,
             'Content-Type': 'application/json'
         },
         body: body,
@@ -61,8 +36,21 @@ function camundaStartProcessSchulungOrganisieren(req, res) {
     request(options, function (error, response, body) {
         if (!error) {
             var bodyStringified = JSON.stringify(body);
-            res.json(bodyStringified);
+            res.json({ "text": "Prozess" + prozess + "gestartet!" });
             console.log(JSON.parse(bodyStringified))
         } else { console.log("ERROR camundaStartProcess: " + error) }
     });
 }
+
+
+const mod = require('./modules');
+
+var name = "QM Schulung";
+var trainer = "Daniel";
+var room = "";
+var date = "2019-05-29T16:00:00.000+0200";
+var businessKey = name + date;
+var path = "/camunda/startProcess/schulungOrganisieren";
+
+var payload = JSON.stringify({ name: name, trainer: trainer, date: date, room: room, businessKey: businessKey });
+mod.postToSwaggerAPI(payload, path);
---
+++ api/controllers/processes.js
@@ -0,0 +1,34 @@
+    [
+        {
+            "schulungOrganisieren": {
+                "name": {
+                    "value": "",
+                    "type": "String"
+                },
+                "trainer": {
+                    "value": "",
+                    "type": "String"
+                },
+                "date": {
+                    "value": "",
+                    "type": "Date"
+                },
+                "room": {
+                    "value": "",
+                    "type": "String"
+                },
+                "process": {
+                    "value": "schulungOrganisieren",
+                    "type": "String"
+                },
+                "correlationKey": {
+                    "value": "name,date",
+                    "type": "String"
+                },
+                "listOfUserIds": {
+                    "value": "SlackIds",
+                    "type": "String"
+                }
+            }
+        }
+    ]
