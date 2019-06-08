9795f09746b9d61a64a8efb16c77ec7915a96d0e
Daniel Köster <danielk.privat@hotmail.de>
2019-06-08 17:22:20 +0200

    update

--- api/controllers/camundaStartProcess.js
+++ api/controllers/camundaStartProcess.js
@@ -25,16 +25,11 @@ async function camundaStartProcess(req, res) {
             break;
         }
     }
+    body.variables.startingUser = {
+        "value": req.swagger.params.user_id.value,
+        "type": "String"
+    };
     console.log(body);
-    body = JSON.parse(body);
-    body.variables.push({
-        "name": {
-            "value": req.swagger.params.user_id.value,
-            "type": "String"
-        }
-    });
-    console.log(body);
-    body = JSON.stringify(body);
     var options = { 
         method: 'POST',
         url: URL,
