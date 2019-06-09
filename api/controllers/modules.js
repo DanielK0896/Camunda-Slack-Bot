f93f6ad2c36fedb4ba539b03ae609972ef986448
Daniel Köster <danielk.privat@hotmail.de>
2019-06-09 11:49:13 +0200

    update

--- api/controllers/modules.js
+++ api/controllers/modules.js
@@ -144,7 +144,7 @@ async function preparePostMessage(task) {
                 }
             }
             if (boldHeadline_index >= 0) {
-                path = "/chat/post/overflow"
+                path = "/chat/post/block"
                 console.log(variables);
                 var fieldInformation = variables[buttonValue_index].split("&%");
                 var stringForActionId = fieldInformation[0].split(",");
