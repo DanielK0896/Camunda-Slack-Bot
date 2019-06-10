6742ddb0f5c9a40963e0aa739422fa1a062641c6
Daniel Köster <danielk.privat@hotmail.de>
2019-06-10 21:14:47 +0200

    update

--- api/controllers/camundaSendMessage.js
+++ api/controllers/camundaSendMessage.js
@@ -11,7 +11,6 @@ module.exports = {
 
 function camundaSendMessage(req, res) {
     var msg = req.swagger.params.body.value;
-    console.log(msg);
     var correlationKeys = msg.correlationKey.split(',');
     var body = {
         "messageName": msg.message,
--- api/controllers/slackReceive.js
+++ api/controllers/slackReceive.js
@@ -275,6 +275,7 @@ function handleMessage(taskid, pushedButton, msg) {
     var path = "/camunda/sendMessage/"
     arrayOfVariables["correlationKey"] = taskid[1];  //callbackId[1] = correlationKeys, look at camundaSendMessage for further Informations
     arrayOfVariables["message"] = taskid[2];        //callbackId[2] = the message name in the camunda process
+    console.log(arrayOfVariables);
     mod.postToSwaggerAPI(arrayOfVariables, path, callback);
 }
 
