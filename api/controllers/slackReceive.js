5216c177d754179ab51b703c4b4055efdc34bc70
Daniel <danielconnect@freenet.de>
2019-06-25 15:23:22 +0200

    fix

--- api/controllers/slackReceive.js
+++ api/controllers/slackReceive.js
@@ -409,13 +409,7 @@ function nextPage(payload, pushedButton, numberOfChanges, taskId) {
             textOptions = "empty";
         }
         var buttonValue = actionIdArray + CAMUNDA_CONFIG.taskIdSplit + leftFieldArray.join(CAMUNDA_CONFIG.leftFieldSplit) + CAMUNDA_CONFIG.taskIdSplit + rightFieldArray.join(CAMUNDA_CONFIG.rightFieldSplit) + CAMUNDA_CONFIG.taskIdSplit + textOptions + CAMUNDA_CONFIG.taskIdSplit + message;
-        console.log(s);
-        console.log(lastElement);
-        console.log(payload.blocks);
-        console.log(payload.blocks.elements);
-        console.log(payload.blocks.elements[lastElement]);
-        console.log(payload.blocks.elements[lastElement].value);
-        payload.blocks[s].elements[lastElement].value = buttonValue[0];
+        payload.blocks[lastBlock].elements[lastElement].value = buttonValue[0];
     }
     console.log(JSON.stringify(payload.blocks));
     payload.blocks = JSON.stringify(payload.blocks);
