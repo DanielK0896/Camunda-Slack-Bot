585be538bfaaab3c77eb8774f37c2f3783083a33
Daniel <danielconnect@freenet.de>
2019-06-28 03:30:41 +0200

    fix

--- api/controllers/slackReceive.js
+++ api/controllers/slackReceive.js
@@ -175,7 +175,7 @@ async function handleMessage(taskId, pushedButton, msg) {
         for (i = 0; i < variableInformation.length; i++) {
             if(variableInformation[i].split(CAMUNDA_CONFIG.propertiesSplit).length > 1) {
                 arrayOfVariables = (mod.pushSpecificVariables(arrayOfVariables, "variable", variableInformation[i], msg, true)); 
-                arrayOfVariables.variable += CAMUNDA_CONFIG.camundaMessageVariablesSplit + pushedButton;
+                arrayOfVariables.variable += CAMUNDA_CONFIG.camundaMessageVariableSplit + pushedButton;
             } else {
                 if(typeof pushedButton == "string") {
                     arrayOfVariables.variable.push(pushedButton);
