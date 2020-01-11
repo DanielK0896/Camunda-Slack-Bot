01144aaf661d1af6e8cfb3e33c05ed9a26d108e0
DanielK96 <danielconnect@freenet.de>
2020-01-11 22:21:44 +0100

    fix

--- api/controllers/mailAuth.js
+++ api/controllers/mailAuth.js
@@ -1,51 +1,40 @@
 /* This file can be reached by calling swagger API endpoint. Pass mail password and local part */
 
-var request = require('request');
-var URL = "https://kasapi.kasserver.com/soap/wsdl/KasAuth.wsdl";
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var URL = "https://kasapi.kasserver.com/soap/wsdl/KasApi.wsdl";
 var secrets = require('../../secrets');
+var soap = require('soap');
 
 module.exports = {
-    addMailaccount: addMailaccount
+    mailAuth: mailAuth
 };
 
-function addMailaccount(req, res) {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('POST', URL, true);
+function mailAuth(req, res) {
+    var params = {
+        kas_login: secrets.kasUserName,
+        kas_auth_type: "sha1",
+        kas_auth_data: secrets.kasPassword,
+        kas_action: "add_mailaccount",
+        mail_password: "Test1",
+        local_part: "test.testQMWORKEND",
+        domain_part: "cct-ev.de",
+        responder: "N"
+    };
 
-    var params = JSON.stringify({
-        KasUser: secrets.kasUserName,
-        KasAuthType: req.localPart,
-        KasPassword: secrets.kasPassword,
-        SessionLifeTime: 600,
-        SessionUpdateLifeTime: "Y",
+soap.createClient(URL, function(err, client) {
+  client.KasApi(params, function(err, result) {
+      console.log(result);
     });
-
-
-    var sr =
-        '<?xml version="1.0" encoding="utf-8"?>' +
-        '<soapenv:Envelope ' + 
-            'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' +
-            'xmlns:api="https://kasapi.kasserver.com/soap/" ' +
-            'xmlns:xsd="http://www.w3.org/2001/XMLSchema" ' +
-            'xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">' +
-            '<soapenv:Body>' +
-                '<api:some_api_call soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">' +
-                    '<KasUser xsi:type="xsd:string">' + secrets.kasUserName + '</KasUser>' +
-                    '<KasAuthType xsi:type="xsd:string">sha1</KasAuthType>' +
-                    '<KasPassword xsi:type="xsd:string">' + secrets.kasPassword + '</KasPassword>' +
-                    '<KasRequestParams xsi:type="xsd:string">' + params + '</KasRequestParams>' +
-                '</api:some_api_call>' +
-            '</soapenv:Body>' +
-        '</soapenv:Envelope>';
-
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState == 4) {
-            if (xmlhttp.status == 200) {
-                console.log(xmlhttp.responseText);
-            }
-        }
-    }
-    xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-    xmlhttp.send(sr);
+});
 }
+
+
+
+
+
+
+
+
+
+
+
+
