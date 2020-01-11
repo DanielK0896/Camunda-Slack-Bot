8a17d3814902046f2eeb6527cb704f8b5b64ec40
DanielK96 <danielconnect@freenet.de>
2020-01-11 16:28:33 +0100

    fix

--- api/controllers/modules.js
+++ api/controllers/modules.js
@@ -413,7 +413,11 @@ function pushSpecificVariables(arrayOfVariables, variableName, variableValue, ms
 
 function getChannels() {
     getFromSwaggerAPI("/slackGet/conversations", function (body) {
-        var bodyParsed = JSON.parse(body);
+        try {
+            var bodyParsed = JSON.parse(body);
+        } catch(e) {
+            console.log("body");
+        }
         for (var i = 0; i < bodyParsed.channels.length; i++) {
             listOfAllChannels = pushSpecificVariables(listOfAllChannels, bodyParsed.channels[i].name, "channels." + i + ".id", bodyParsed);            
         }
