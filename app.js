d2683581f8e56e8b2790cfc995090396574e1c26
DanielK96 <danielconnect@freenet.de>
2020-01-11 17:56:01 +0100

    fix

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   app.listen(port);
 });
 
-setTimeout(mod.getChannels, 1000);
+setTimeout(mod.getChannels, 3000);
 setInterval(mod.getChannels, 300000);
 //setTimeout(mod.getUsers, 500);
 //setInterval(mod.getUsers, 300000);
