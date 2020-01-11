85cf0ac5dc97b1c06480f106bfd8a8b6543e4edf
DanielK96 <danielconnect@freenet.de>
2020-01-11 21:27:55 +0100

    fix

--- api/controllers/addMailaccount.js
+++ api/controllers/addMailaccount.js
@@ -20,7 +20,7 @@
         };
 
     soap.createClient(URL, function(err, client) {
-      client.KasApi(params, function(err, result) {
+      client.kasAuth(params, function(err, result) {
           console.log(result);
         });
     });
